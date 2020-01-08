745049483404de2bc281f2569604e11cd153c8d4
Fabian Alberto Serna Salazar <fabian.serna@PRAGMA.COM.CO>
2020-01-08 14:24:52 -0500

    intervalo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ComicsService } from '../providers/comics/comics.service';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,38 @@ import { ComicsService } from '../providers/comics/comics.service';
 })
 export class HomePage {
 
-  constructor( private comicsService: ComicsService ) {
+  @ViewChild(IonContent) content: IonContent;
 
-    console.log(this.comicsService.comicsData);
+  availableComics: Array<JSON>;
+  interval: number;
 
+  constructor( private comicsService: ComicsService) {
+
+    this.availableComics = this.comicsService.loadComics(1);
+    this.interval = this.countIntervals(this.comicsService.getnComics());
+    console.log(this.interval)
+
+  }
+
+  createLink(path, extension){
+    return `${path}.${extension}`;
+  }
+
+  loadMoreComics(ev: any){
+    this.availableComics = this.comicsService.loadComics(ev['detail'].value);
+    this.content.scrollToTop();
+  }
+
+  countIntervals(nComics){
+    var interval = nComics / 6;
+    if(parseInt(interval) < interval) {
+      var interval = parseInt(interval) + 1;
+    }
+    return interval;
+  }
+
+  counter(){
+    return Array(this.interval - 1);
   }
 
 }
